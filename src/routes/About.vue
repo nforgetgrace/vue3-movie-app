dc8b00e1fe187110fcae9622ced4ed0fde67d0b3
nforgetgrace <nforgetgrace@gmail.com>
2023-02-01 09:49:41 +0900

    no message

--- src/routes/About.vue
+++ src/routes/About.vue
@@ -1,5 +1,9 @@
 <template>
-  <h1> About! </h1>
+  <div class="wrapper">
+    <div class="typing-demo">
+      This is a typing demo.
+    </div>
+  </div>
 </template>
 
 <script>
@@ -8,6 +12,33 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+.wrapper {
+  height: 100vh;
+  /*This part is important for centering*/
+  display: grid;
+  place-items: center;
+}
 
-</style>
+.typing-demo {
+  width: 22ch;
+  animation: typing 5s steps(22), blink .5s step-end infinite alternate;
+  white-space: nowrap;
+  overflow: hidden;
+  border-right: 3px solid;
+  font-family: monospace;
+  font-size: 2em;
+}
+
+@keyframes typing {
+  from {
+    width: 0
+  }
+}
+    
+@keyframes blink {
+  50% {
+    border-color: transparent
+  }
+}
+</style>>
